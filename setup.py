b3030c83945864753d52c684d9ecaec422dbe49f
Fred Drake <fdrake@acm.org>
2006-09-06 19:07:40 +0000

    add reasonable metadata

--- setup.py
+++ setup.py
@@ -3,8 +3,14 @@ from setuptools import setup
 setup(
     name="zc.queue",
     version="1.0",
-    package_dir={"": "src"},
+    license="ZPL 2.1",
+    author="Gary Poster",
+    author_email="gary@zope.com",
 
+    namespace_packages=["zc"],
+    packages=["zc", "zc.queue"],
+    package_dir={"": "src"},
+    include_package_data=True,
     install_requires=["zope.interface", "ZODB3"],
     tests_require=["zope.testing"],
     )
