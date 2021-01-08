956b28964b3091aea94d0a2fe7d2f53e09411fad
Michael Howitz <mh@gocept.com>
2021-01-08 08:29:42 +0100

    Add support for Python 3.7, 3.8 and 3.9. (#3)
    * Configuring for pure-python
    * Add support for Python 3.7, 3.8 and 3.9.
    * Drop support for Python 3.4.
    * Lint the code.
    
    Co-authored-by: Marius Gedminas <marius@gedmin.as>

--- setup.py
+++ setup.py
@@ -16,9 +16,11 @@
 import os
 from setuptools import setup
 
+
 def read(*rnames):
     with open(os.path.join(os.path.dirname(__file__), *rnames)) as f:
         return f.read()
+
 
 tests_require = [
     "zope.testing",
@@ -27,14 +29,14 @@ tests_require = [
 
 setup(
     name="zc.queue",
-    version='2.0.1.dev0',
+    version='2.1.0.dev0',
     author="Zope Project",
     author_email="zope-dev@zope.org",
     description=read('README.rst'),
     long_description='\n\n'.join([
         read('src', 'zc', 'queue', 'queue.rst'),
         read('CHANGES.rst'),
-        ]),
+    ]),
     keywords='zope zope3',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
@@ -44,16 +46,18 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Framework :: Zope :: 3'
-        ],
-    url='http://github.com/zopefoundation/zc.queue',
+    ],
+    url='https://github.com/zopefoundation/zc.queue',
     license="ZPL 2.1",
     namespace_packages=['zc'],
     packages=['zc', 'zc.queue'],
